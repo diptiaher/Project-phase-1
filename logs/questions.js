7065d2df34f7d89f64b79f0092769978f6ff813a
diptiaher <diptiaher1597@gmail.com>
2021-08-12 15:05:11 +0530

    my project

---
+++ logs/questions.js
@@ -0,0 +1,96 @@
+let questions = [
+    {
+    numb: 1,
+    question: " Which among the following Kavya of Sanskrit, deal with court intrigues & access to power of Chandragupta Maurya",
+    answer: "D. Mudrarakshahsa",
+    options: [
+      "A. Mrichhakatika",
+      "B. Ritusamhara",
+      "c. Kumarasambhava",
+      "D. Mudrarakshahsa"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which among the following is not correct?",
+    answer: "C. Capital of the Videha Kingdom – Mithila",
+    options: [
+      "A. The capital of pandyas was Madurai",
+      "B. The capital of Cheras was Vanchi",
+      "C. Capital of the Videha Kingdom – Mithila",
+      "D. Capital of Gahadwal Dynasty – Kannauj"
+    ]
+  },
+
+  {
+    numb: 3,
+    question: "Upnishads are books on ?",
+    answer: "B. Philosophy",
+    options: [
+      "A. Politics",
+      "B. Philosophy",
+      "C. Medicine",
+      "D. None of the above."
+      
+    ]
+  },
+    {
+    numb: 4,
+    question: "Who among the following was worshipped during Early Vedic Civilization?",
+    answer: "D. All the above.",
+    options: [
+      "A. Varuna",
+      "B. Indra.",
+      "C. Surya",
+      "D. All the above."
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which among the following is a place in Larkana district of Sind province in Pakistan?",
+    answer: "D. Mohenjo-Daro.",
+    options: [
+      "A. Alamgirpur.",
+      "B. Harappa",
+      "C. Rangapur.",
+      "D. Mohenjo-Daro."
+    ]
+  },
+
+    {
+    numb: 6,
+    question: "Who was the last Hindu emperor of northern India?",
+    answer: "A. Harsha",
+    options: [
+    "A. Harsha",
+    "B. Pulakesin II",
+    "C. Rajyavardhana",
+    "D. Skandagupta"
+    ]
+  },
+  
+   {
+    numb: 7,
+    question: "Bimbisara was succeeded by.",
+    answer: "C. Ajatashatru.",
+    options: [
+    "A. Chandragupta Maurya",
+    "B. vasudeva",
+    "C. Ajatashatru.",
+    "D. Ashoka"
+   ]
+   },
+
+   {
+    numb: 8,
+    question: "The fourth Buddhist Council was held at",
+    answer: "B. Kundalavana.",
+    options: [
+    "A. Vaishali.",
+    "B. Kundalavana.",
+    "C. Bodh Gaya",
+    "D. pataliputra"
+   ]
+   },
+
+];
